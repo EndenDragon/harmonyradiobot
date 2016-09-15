09f2b5e48d131c658624d13056977897ec3d0763
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-09-14 19:12:43 -0700

    Search and request songs

--- config.example.py
+++ config.example.py
@@ -11,3 +11,13 @@ MUSIC_CHANNEL = 265924908423
 MUSIC_STREAM_URL = "http://epsilon.shoutca.st:8505/stream"
 
 METADATA_URL = "http://epsilon.shoutca.st:8505/stats"
+
+SONG_TRACKS_URL = "https://epsilon.shoutca.st/client/rpc.php?m=playlist.get_tracks&p%5B%5D=2134"
+
+CENTOVACAST_USERNAME = "Rainbow"
+
+CENTOVACAST_PASSWORD = "Dash"
+
+CENTOVACAST_LOGIN_URL = "https://epsilon.shoutca.st/login/index.php"
+
+CENTOVACAST_REQUEST_URL = "https://epsilon.shoutca.st/external/rpc.php"
--- main.py
+++ main.py
@@ -9,18 +9,26 @@ import subprocess
 import time
 import datetime
 import sys
+import requests
+import json
 
 client = discord.Client()
 logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger('HarmonyBot')
 
 currentDate = datetime.datetime.now().date()
+centovaCookie = ""
 
 def getRadioSong():
     response = urlopen(METADATA_URL)
     xsl = response.read()
     hr_json = str(xsl.decode("utf-8"))
     return hr_json[hr_json.find("<SONGTITLE>")+11:hr_json.find("</SONGTITLE>")]
+
+def centovaGetLoginCookie(url, username, password):
+    payload = {'username': username, 'password': password, 'login': 'Login'}
+    r = requests.head(url, data=payload, allow_redirects=False)
+    return r.cookies['centovacast']
 
 @client.event
 async def on_ready():
@@ -36,6 +44,8 @@ async def on_ready():
         print(x.name)
     print('------')
     radioMeta = ""
+    global centovaCookie
+    centovaCookie = centovaGetLoginCookie(CENTOVACAST_LOGIN_URL, CENTOVACAST_USERNAME, CENTOVACAST_PASSWORD)
     global currentlyStreaming
     currentlyStreaming = False
     c = discord.utils.get(client.get_server(str(MAIN_SERVER)).channels, id=str(MUSIC_CHANNEL), type=discord.ChannelType.voice)
@@ -68,6 +78,8 @@ async def on_message(message):
         `!help` - displays this help menu
         `!about` - general information about HarmonyRadioBot
         `!nowplaying` - shows what is currently playing in the station
+        `!search <query>` - search for a song's title or author with the given string
+        `!request <id>` - make a song request to the station
         ----------------------
         `!joinvoice` - joins the voice channel with the person who sent the command
         `!disconnectvoice` - disconnect from the voice channel
@@ -91,6 +103,50 @@ async def on_message(message):
         hr_txt = getRadioSong()
         text = "**Now Playing:** " + str(hr_txt)
         await client.send_message(message.channel, text)
+    elif message.content.startswith('!search'):
+        await client.send_typing(message.channel)
+        if len(str(message.content)) == 7:
+            await client.send_message(message.channel, "**I'm sorry, what was that? Didn't quite catch that.** \n Please enter your search query after the command. \n eg. `!search Rainbow Dash`")
+        else:
+            cookies = {'centovacast': centovaCookie}
+            songfile = json.loads(requests.get(SONG_TRACKS_URL, cookies=cookies).text)
+            songs = songfile['data'][1]
+            artists = songfile['data'][2]
+            query = str(message.content).split(' ', 1)[1]
+            count = 0
+            botmessage = "**__Search Songs: " + query + "__**\n[ID | Artist | Title]\n"
+            for element in songs:
+                if count < 10:
+                    if query in str(artists['i' + str(element['artistid'])]).lower() or query in element['title'].lower():
+                        botmessage = botmessage + "**" + str(element['id']) + "** | " + artists['i' + str(element['artistid'])] + " | " + element['title'] + "\n"
+                        count = count + 1
+                else:
+                    break
+            await client.send_message(message.channel, botmessage)
+    elif message.content.startswith('!request') or message.content.startswith('!req'):
+        await client.send_typing(message.channel)
+        msg = message.content.split(" ", 1)
+        if len(msg) == 1:
+            await client.send_message(message.channel, "**I just don't know what went wrong!** \n Please enter your requested song id after the command. \n eg. `!request 14982` \n _Remember: you can search for the song with the `!search` command!_")
+        else:
+            cookies = {'centovacast': centovaCookie}
+            songfile = json.loads(requests.get(SONG_TRACKS_URL, cookies=cookies).text)
+            songs = songfile['data'][1]
+            artists = songfile['data'][2]
+            idquery = msg[1]
+            status = False
+            for element in songs:
+                print(str(idquery) + str(element['id']))
+                if str(idquery) == str(element['id']):
+                    req = requests.get(CENTOVACAST_REQUEST_URL+ "?m=request.submit&username=ponyharmony&artist={0}&title={1}&sender={2}&email=sssss@ss.com&dedi=myself".format(artists['i' + str(element['artistid'])],  element['title'], message.author.name))
+                    print(req.url)
+                    j = json.loads(req.text)
+                    if j['type'] == "result":
+                        status = True
+                        await client.send_message(message.channel, j['data'][0])
+                        break
+            if status == False:
+                await client.send_message(message.channel, "Song ID not found!")
     elif message.content.startswith('!joinvoice') or message.content.startswith('!jv'):
         await client.send_typing(message.channel)
         if int(str(message.author.id)) in BOT_ADMINS or int(str(message.author.voice_channel.id)) in TRUSTED_VOICE_CHANNELS:
